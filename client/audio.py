ff06d8944cd9ba86994024b336ee31eb44fc5952
Nathan Rossow <nathanrossow@gmail.com>
2024-09-01 12:18:32 -0700

    Create audio.py

---
+++ client/audio.py
@@ -0,0 +1,9 @@
+class AudioHandler:
+    def __init__(self):
+        pass
+
+    def encode_audio(self, audio_data):
+        pass  # Implement audio encoding
+
+    def decode_audio(self, audio_data):
+        pass  # Implement audio decoding
